2068a7b342dc42d8105ec7ad7a1d4f153dc23c76
dhyey <dhyeythumar@gmail.com>
2021-06-19 15:14:51 +0530

    fix: update pkg version

--- gym_video_streamer/__init__.py
+++ gym_video_streamer/__init__.py
@@ -2,7 +2,7 @@
 # website:	https://github.com/dhyeythumar/gym-video-streamer
 
 # set the version number
-__version__ = "1.0.0"
+__version__ = "1.1.1"
 
 from .setupVirtualDisplay import SetupVirtualDisplay
 from .gymVideoStreamingWrapper import VideoStreamingWrapper as VideoStreamer
