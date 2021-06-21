bee344ad6776cd8556cb44bd926cc953c8c3a162
dhyey <dhyeythumar@gmail.com>
2021-06-21 11:28:14 +0530

    add note on versioning

--- gym_video_streamer/__init__.py
+++ gym_video_streamer/__init__.py
@@ -2,7 +2,13 @@
 # website:	https://github.com/dhyeythumar/gym-video-streamer
 
 # set the version number
-__version__ = "1.1.1"
+__version__ = "1.0"
 
 from .setupVirtualDisplay import SetupVirtualDisplay
 from .gymVideoStreamingWrapper import VideoStreamingWrapper as VideoStreamer
+
+# --- Note on versioning ---
+# Only minor changes & patches should be released under v1.0 as v1.0.0, v1.0.1, v1.0.2...
+
+# v1.1.0 & v1.1.1 won't work as they are already used because of my silliness 😥.
+# So just use v2.x.x
